d5fcf48a246b663b9c411481a7dae18dfc270819
AN <contact.aidin@gmail.com>
2020-11-30 18:05:04 -0800

    Testing if periods active class change when clicked

--- src/Timeline.spec.ts
+++ src/Timeline.spec.ts
@@ -7,4 +7,22 @@ describe("Timeline", () => {
 
     expect(wrapper.findAll('[data-test="period"]')).toHaveLength(3);
   });
+
+  it("Updates the period when clicked", async () => {
+    const wrapper = mount(Timeline);
+    const $today = wrapper.findAll('[data-test="period"]')[0];
+    expect($today.classes()).toContain("is-active");
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1];
+    await $thisWeek.trigger("click");
+
+    expect($today.classes()).not.toContain("is-active");
+    expect($thisWeek.classes()).toContain("is-active");
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2];
+    await $thisMonth.trigger("click");
+
+    expect($thisWeek.classes()).not.toContain("is-active");
+    expect($thisMonth.classes()).toContain("is-active");
+  });
 });
