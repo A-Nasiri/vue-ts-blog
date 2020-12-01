d5463098776ab87751fb6f75689c94ffc05b7cf3
AN <contact.aidin@gmail.com>
2020-11-30 17:24:37 -0800

    Timeline component first test

--- src/Timeline.vue
+++ src/Timeline.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="is-primary panel">
     <p class="panel-tabs">
-      <a v-for="period in periods" :key="period">
+      <a v-for="period in periods" :key="period" data-test="period">
         {{ period }}
       </a>
     </p>
@@ -11,9 +11,11 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Period } from "./types";
+
 export default defineComponent({
   setup() {
     const periods: Period[] = ["today", "this week", "this month"];
+
     return {
       periods
     };
