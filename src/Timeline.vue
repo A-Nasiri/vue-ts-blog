13c713c98964bbcd0b101ac93cbab7ca8594e24f
AN <contact.aidin@gmail.com>
2020-11-30 17:49:25 -0800

    Make the active period update dynamically

--- src/Timeline.vue
+++ src/Timeline.vue
@@ -1,7 +1,13 @@
 <template>
   <nav class="is-primary panel">
     <p class="panel-tabs">
-      <a v-for="period in periods" :key="period" data-test="period">
+      <a
+        v-for="period in periods"
+        :key="period"
+        data-test="period"
+        :class="[period === selectedPeriod ? 'is-active' : '']"
+        @click="setPeriod(period)"
+      >
         {{ period }}
       </a>
     </p>
@@ -9,15 +15,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { Period } from "./types";
 
 export default defineComponent({
   setup() {
     const periods: Period[] = ["today", "this week", "this month"];
+    const selectedPeriod = ref<Period>("today");
+
+    const setPeriod = (period: Period) => {
+      selectedPeriod.value = period;
+    };
 
     return {
-      periods
+      periods,
+      selectedPeriod,
+      setPeriod
     };
   }
 });
