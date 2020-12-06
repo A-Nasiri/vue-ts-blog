7328fc5020bb8d20ea4b323f0436ebf813212e4b
AN <contact.aidin@gmail.com>
2020-12-05 18:20:50 -0800

    New Post route and page

---
+++ src/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-end">
+      <div class="buttons">
+        <router-link class="button" to="/posts/new">New Post</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
